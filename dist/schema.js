f14495843f45be616085209189ff8fa60a564e37
Eigil Sagafos <eigil@sagafos.no>
2020-06-09 20:55:20 +0200

    Build schema and mutation

--- dist/schema.js
+++ dist/schema.js
@@ -8,6 +8,38 @@ let __export = (target, all) => {
     __defineProperty(target, name, {get: all[name], enumerable: true});
 };
 
+// src/modules/foo/queries.ts
+const queries_exports = {};
+__export(queries_exports, {
+  FooQuery1: () => FooQuery1,
+  FooQuery2: () => FooQuery2,
+  FooQuery3: () => FooQuery3,
+  FooQuery4: () => FooQuery4
+});
+const FooQuery1 = {};
+const FooQuery2 = {};
+const FooQuery3 = {};
+const FooQuery4 = {};
+
+// src/modules/bar/queries.ts
+const queries_exports = {};
+__export(queries_exports, {
+  BarQuery1: () => BarQuery1,
+  BarQuery2: () => BarQuery2,
+  BarQuery3: () => BarQuery3,
+  BarQuery4: () => BarQuery4
+});
+const BarQuery1 = {};
+const BarQuery2 = {};
+const BarQuery3 = {};
+const BarQuery4 = {};
+
+// src/query.ts
+const query = {
+  ...queries_exports,
+  ...queries_exports
+};
+
 // src/modules/foo/mutations.ts
 const mutations_exports = {};
 __export(mutations_exports, {
@@ -22,10 +54,13 @@ const FooMutation3 = {};
 const FooMutation4 = {};
 
 // src/mutation.ts
-__export(exports, {
-  mutation: () => mutation
-});
 const mutation = {
   ...mutations_exports,
   ...mutations_exports
+};
+
+// src/schema.ts
+const schema = {
+  query,
+  mutation
 };
